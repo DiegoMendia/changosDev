d74d7fd3673d0d201a04a6e2533460f3db564c28
DiegoMendia <diegomendia21@gmail.com>
2025-10-23 00:30:05 -0300

    agrego cambios de section equipo

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,20 @@ botones.forEach(function(boton) {
     }
   });
 });
+
+
+document.querySelectorAll('.leer-mas-btn').forEach(button => {
+      button.addEventListener('click', (e) => {
+        e.stopPropagation();
+        const card = button.closest('.profile-card');
+        const additionalDesc = card.querySelector('.additional-desc');
+        
+        if (additionalDesc.classList.contains('show')) {
+          additionalDesc.classList.remove('show');
+          button.textContent = 'Leer más';
+        } else {
+          additionalDesc.classList.add('show');
+          button.textContent = 'Leer menos';
+        }
+      });
+    });
